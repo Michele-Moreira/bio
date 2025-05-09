232a02f237bb093e297432e68a6e9ad8c4195b7b
Michele Moreira <mikaut.silva@gmail.com>
2025-05-09 18:22:22 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    // se tiver light mode, adicionar legenda no alt
+    img.setAttribute('alt', 'Foto de perfil da Michele com fundo colorido')
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+      //se tiver sem light mode, adicionar legenda no alto
+      img.setAttribute('alt', 'Foto de perfil da Michele com fundo laranja')
+  }
+}
+  
